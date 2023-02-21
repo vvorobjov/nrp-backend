2cac5d73060546d37e0da0a9585251923f4c5467
Viktor Vorobev <vorobev@in.tum.de>
2023-02-21 12:08:26 +0100

    [NRRPLT-8797] The entrypoint is added to handle SIGTERM

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,32 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -x
 
+pid=0
+
+
+# SIGTERM-handler
+term_handler() {
+  if [ $pid -ne 0 ]; then
+    sudo nginx -s stop
+    kill -SIGTERM "$pid"
+    wait "$pid"
+  fi
+  exit 143; # 128 + 15 -- SIGTERM
+}
+
+# setup handlers
+# on callback, kill the last background process, which is `tail -f /dev/null` and execute the specified handler
+trap 'kill ${!}; term_handler' SIGTERM
+
+# start nginx
 sudo nginx
-uwsgi --ini /usr/uwsgi-nrp.ini --plugin python3 --pyargv "--verbose"
+
+# run application
+uwsgi --ini /usr/uwsgi-nrp.ini --plugin python3 --pyargv "--verbose" &
+pid="$!"
+
+# wait forever
+while true
+do
+  tail -f /dev/null & wait ${!}
+done
